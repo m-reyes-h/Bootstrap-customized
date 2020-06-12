cd858057bac19f94589290e0de90de5ad3d18065
Michel Reyes <mreyes.girosol@gmail.com>
2020-06-12 11:44:28 -0400

    added tabs

--- src/components/Tabular.js
+++ src/components/Tabular.js
@@ -5,29 +5,53 @@ import { BsCollection as CollectionIcon} from "react-icons/bs";
 const First = () => {
   return (
     <>
-  <CollectionIcon /> Hello
-  </>);
+      <CollectionIcon size="20" className="mr-2" /> Hello
+    </>
+ );
 }
 
 function Tabular() {
   const [key, setKey] = useState("home");
   
   return (
-    <Tabs
-      id="controlled-tab-example"
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-    >
-      <Tab eventKey="home" title={<First />}>
-        som data 1
-      </Tab>
-      <Tab eventKey="profile" title="Profile">
-        som data 12
-      </Tab>
-      <Tab eventKey="contact" title="Contact" disabled>
-        som data 13
-      </Tab>
-    </Tabs>
+    <>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="home" title={<First />}>
+          som data 1
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          som data 12
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          som data 13
+        </Tab>
+      </Tabs>
+
+      <h4 className="mt-4">Primary</h4>
+
+      <Tabs
+        className="primary"
+        id="controlled-tab1-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="home" title={<First />}>
+          som data 1
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          som data 12
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          som data 13
+        </Tab>
+      </Tabs>
+
+      
+    </>
   );
 }
 
